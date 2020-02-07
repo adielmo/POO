2394d53f058d40e74067a08f267321ea26ca2ac4
adielmo <adielmorabelo@gmail.com>
2020-02-07 11:48:58 -0300

    Extraindo os metodos fora da Class Main

--- src/com/java/matrizes/ex/Data.java
+++ src/com/java/matrizes/ex/Data.java
@@ -5,20 +5,30 @@ public class Data {
 		
 		Integer[][] mat = new Integer[3][4]; 
 
-		for (int i = 0; i < mat.length; i++) {
-
-			for (int j = 0; j < mat.length; j++) {
-
-				mat[i][j] = ((i +j) % 3);
-			}
-		}
+		entradaDeDados(mat);
 		
+		saidaDeDados(mat);
+		
+		System.out.println("Fim do Programa!!");
+	}
+
+	private static void saidaDeDados(Integer[][] mat) {
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat.length; j++) {
 				System.out.print(mat[i][j] + " ");
 				
 			}
 			
+		}
+	}
+
+	private static void entradaDeDados(Integer[][] mat) {
+		for (int i = 0; i < mat.length; i++) {
+
+			for (int j = 0; j < mat.length; j++) {
+
+				mat[i][j] = ((i +j) % 3);
+			}
 		}
 	}
 }

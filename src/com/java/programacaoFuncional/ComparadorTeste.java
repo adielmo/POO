525fcb6ea90f0345c3fd9e3023cd51a031e802ea
adielmo <adielmorabelo@gmail.com>
2020-02-28 21:28:49 -0300

    Alterando o Recurso Java 8 Stream, Reduce & Filter

--- src/com/java/programacaoFuncional/ComparadorTeste.java
+++ src/com/java/programacaoFuncional/ComparadorTeste.java
@@ -2,6 +2,8 @@ package com.java.programacaoFuncional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ComparadorTeste {
 	public static void main(String[] args) {
@@ -10,7 +12,7 @@ public class ComparadorTeste {
 
 		Produto p1 = new Produto("Feijão", 12.57, 12);
 		Produto p2 = new Produto("Arroz", 15.78, 19);
-	/*	Produto p3 = new Produto("Acuçar", 2.49, 7);
+		Produto p3 = new Produto("Acuçar", 2.49, 7);
 		Produto p4 = new Produto("Café", 11.75, 23);
 		Produto p5 = new Produto("Cerveja", 4.98, 38);
 		Produto p6 = new Produto("Refrigerante", 7.89, 41);
@@ -19,32 +21,39 @@ public class ComparadorTeste {
 		Produto p9 = new Produto("Desodorante", 15.71, 75);
 		Produto p10 = new Produto("Espaguete", 3.75, 62);
 		Produto p11 = new Produto("Queijo Prato", 27.89, 56);
-		Produto p12 = new Produto("Picanha Bovina", 49.75, 15);*/
-		//List<Produto> list = Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12);		
+		Produto p12 = new Produto("Picanha Bovina", 49.75, 15);
+		// List<Produto> list = Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10,
+		// p11, p12);
+
+		list.add(p1);
+		list.add(p2);
+		list.add(p3);
+		list.add(p4);
+		list.add(p5);
+		list.add(p6);
+		list.add(p7);
+		list.add(p8);
+		list.add(p9);
+		list.add(p10);
+		list.add(p11);
+		list.add(p12);
+
+		// list.sort((a1, a2) ->
+		// a1.getNome().toUpperCase().compareTo(a2.getNome().toUpperCase()));
+		// .sorted((a1, a2) -> a1.getNome().toUpperCase().compareTo(a2.getNome().toUpperCase()))
 		
-		  list.add(p1);
-		  list.add(p2);
-	/*	  list.add(p3);
-		  list.add(p4);
-		  list.add(p5);
-		  list.add(p6);
-		  list.add(p7);
-		  list.add(p8);
-		  list.add(p9);
-		  list.add(p10);
-		  list.add(p11);
-		  list.add(p12);		*/	
-		  
-	/*	  int total =0;		  
-		  for (int i = 0; i < list.size(); i++) {
-			total += list.get(i).getQtd();
-		}*/
-  //int total = list.stream().reduce(0,(x, y) -> x + y.getQtd(), Integer::sum);
-    double total = list.stream().mapToDouble(Produto::getPreco).reduce(0,(x, y) -> x * y);	  
-		  
-		  System.out.println(total);
-		
-		
+		double total = list.stream()				
+				.filter(x1 -> x1.getNome().toUpperCase().charAt(0) == 'A')
+				.mapToDouble(Produto :: getPreco)
+		        .reduce((a, b) -> a + b).orElse(0);
+
+	System.out.println(total);
+	list.forEach(System.out::println);
+
+		// System.out.println(String.format("%.2f\n", tt));
+
+//list.forEach(System.out::println);
+
 	}
 
 }
